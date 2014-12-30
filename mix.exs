732135a4037579a5e6fcd106aa5a52605018d72d
Blake Williams <blake@blakewilliams.me>
2014-12-29 19:27:07 -0500

    Get lib ready for hex.pm

--- mix.exs
+++ mix.exs
@@ -5,7 +5,10 @@ defmodule Cleverbot.Mixfile do
     [app: :cleverbot,
      version: "0.0.1",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     source_url: "https://github.com/BlakeWilliams/Elixir-Cleverbot",
+     description: "A Cleverbot API wrapper.",
+     package: package]
   end
 
   def application do
@@ -16,5 +19,14 @@ defmodule Cleverbot.Mixfile do
     [{:httpoison, "~> 0.5.0"},
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.6", only: :dev}]
+  end
+
+  defp package do
+    %{contributors: ["Blake Williams"],
+      licenses: ["MIT"],
+      links: %{
+        "Github": "https://github.com/BlakeWilliams/Elixir-Cleverbot",
+        "Documentation": "http://hexdocs.pm/cleverbot/"
+      }}
   end
 end
